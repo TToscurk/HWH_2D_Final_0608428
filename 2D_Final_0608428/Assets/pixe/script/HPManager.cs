ec6e321499229dcbd09fb0be014b80b3b1232f06
TToscurk <71476476+TToscurk@users.noreply.github.com>
2021-05-28 06:17:51 +0800

    期末5/28

--- 2D_Final_0608428/Assets/pixe/script/HPManager.cs
+++ 2D_Final_0608428/Assets/pixe/script/HPManager.cs
@@ -27,14 +27,14 @@ public class HPManager : MonoBehaviour
     public IEnumerator ShowDamage(float damage)
     {
         RectTransform rect = Instantiate(rectDamage, transform); //生成傷害數值在血條系統內
-        rect.anchoredPosition = new Vector2(0, 200);//指定座標
+        rect.anchoredPosition = new Vector2(0, 0);//指定座標
         rect.GetComponent<Text>().text = damage.ToString();//更新數值
 
         float y = rect.anchoredPosition.y; //取得原址Y軸
         while(y<400)//當Y小於 400時 持續執行
 
         {
-            y += 20;//每次上升的單位20
+            y += 3;//每次上升的單位20
             rect.anchoredPosition = new Vector2(0, y);//更新座標
             yield return new WaitForSeconds(0.02f);//等待幾秒(秒數)
 
